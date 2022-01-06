1002230d781e094cdc06397f30217291b98b7d8e
shame <shame.bdao@gmail.com>
2022-01-06 23:46:22 +1100

    Added keyboard input functionality and carry-over evaluation

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 const maxChars = 12;
+let lastBtnPressed;
 
 let answerNum;
 let operation;
@@ -27,6 +28,10 @@ opBtns.forEach(opBtn => {
 
 equalsBtn.addEventListener("click", () => evaluate(answerNum, tempNumStr, operation));
 
+// listen for keypresses
+document.addEventListener("keydown", typeInput);
+
+
 function resetCalc() {
     answerNum = "";
     operation = "";
@@ -45,11 +50,19 @@ function backspace() {
     }
 }
 
-function storeNum(event) {
+function storeNum(event, typed = false) {
     // check if max digits has been reached
-    if (tempNumStr.length >= maxChars) return;
+    if (tempNumStr.length >= maxChars) {
+        return;
+    }
 
-    let char = event.target.textContent;
+    let char;
+    
+    if (typed) {
+        char = event;
+    } else {
+        char = event.target.textContent;
+    }
     console.log(char);
 
     if (char === "." && tempNumStr === "") tempNumStr = "0.";
@@ -58,15 +71,21 @@ function storeNum(event) {
     screenBottom.textContent = tempNumStr;
 }
 
-function storeOp(event) {
+function storeOp(event, typed = false) {
     if (answerNum && tempNumStr && operation) { // evaluate without "="
         answerNum = operate(operation, +answerNum, +tempNumStr);
         screenBottom.textContent = answerNum;
         tempNumStr = "";
 
     }
-    let char = event.target.textContent;
-    console.log(char);
+
+    let char;
+    
+    if (typed) {
+        char = event;
+    } else {
+        char = event.target.textContent;
+    }
     operation = char;
 
 
@@ -117,6 +136,12 @@ function operate(operator, a, b) {
 function evaluate(answerNum, tempNumStr, operation) {
     // check if both numbers exist before evaluating
     if (!(answerNum && tempNumStr && operation)) return;
+
+    // check if evaluate has just been used prior
+    if (screenTop.textContent.includes("=")) {
+        // Use carry-over evaluate
+        answerNum = screenBottom.textContent;
+    }
     
     let numOne = +answerNum;
     let numTwo = +tempNumStr;
@@ -127,18 +152,45 @@ function evaluate(answerNum, tempNumStr, operation) {
     answerNum = roundNumber(answerNum);
     screenBottom.textContent = answerNum;
     console.log(`=\n${answerNum}`)
+    
 
-    tempNumStr = "";
-    operation = "";
+    // tempNumStr = "";
+    // operation = "";
 
 }
 
 // round number to 10 dp 
 function roundNumber(number) {
+    
     if (number.toString().length > maxChars) {
         console.log(number);
         return (+number).toFixed(maxChars - 2);
     } else {
         return number;
+    }
+}
+
+function typeInput(event) {
+    let keyPressed = event.key;
+    if (/[0-9]/.test(keyPressed)) storeNum(keyPressed, true);
+    else if (/[-+*\/x]/.test(keyPressed)) {
+        switch (keyPressed) {
+            case "*":
+            case "x":
+                keyPressed = "×";
+                break;
+            case "/":
+                keyPressed = "÷";
+                break;
+
+            default:
+                break;
+        }
+        storeOp(keyPressed, true);
+    } 
+    else if (keyPressed === "=" || keyPressed === "Enter") {
+        evaluate(answerNum, tempNumStr, operation);
+    } else if (keyPressed === "Escape") {
+        resetCalc();
     }
 }
